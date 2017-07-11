7b4f33f3927b6eb6d25edab0039131b720bae99b
mcanmwoo <mcanmutaf@outlook.com>
2017-07-11 09:48:31 +0300

    frst

---
+++ Assets/Additem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Additem : MonoBehaviour {
+
+	public GameObject text;
+
+
+public Text first;
+
+public Text input;
+
+
+public void	buttonEvent()
+	{
+		
+		//input.text = "AAG";
+
+		first.text = "1." + input.text;
+ 
+text.SetActive(true);
+
+
+
+
+}
+
+
+}
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+
+
+
+public GameObject text;
+	// Use this for initialization
+	void Start () {
+
+
+
+text.SetActive(false);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
